23c5df8d16db7dc602afd9ead609ab1559bd885f
Dari Stephen <daristephen999@gmail.com>
2023-08-04 17:27:53 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,16 +4,15 @@
  * set_bit - a given index of a bit to set
  * @n: change the number to the pointer
  * @index: 1 is to be set to the index bit
+ * Return: if success return 1 and -1 if it fails
+ *
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 if (index > 63)
-
 return (-1);
 
-
 *n = ((1UL << index) | *n);
-
 return (1);
 }
